d1df94120a655f7f414a5c165532f89e211ae722
Huỳnh Lê Minh Trí <hlmtri@mac09.local>
2017-05-31 16:36:33 +0700

    Initial Commit

---
+++ Momov3Tests/Momov3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Momov3Tests.swift
+//  Momov3Tests
+//
+//  Created by Huỳnh Lê Minh Trí on 5/31/17.
+//  Copyright © 2017 cusc. All rights reserved.
+//
+
+import XCTest
+@testable import Momov3
+
+class Momov3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
